d0b1065ab2eef66c1cc7a89eefb3fa65ab04a59d
Ivan Ivchenko <ivan.ivchenko@betssongroup.com>
2018-03-28 09:56:42 +0300

    Asp.Net MVC 5 Metanit

---
+++ metanit/BookStore/BookStore/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+using BookStore.Models;
+
+namespace BookStore.Controllers
+{
+    public sealed class HomeController : Controller
+    {
+        private readonly BookContext _db;
+
+        public HomeController()
+        {
+            _db = new BookContext();
+        }
+
+        public ActionResult Index()
+        {
+            ViewBag.Books = _db.Books;
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+
+            _db.Purchases.Add(purchase);
+            _db.SaveChanges();
+
+            return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+    }
+}
---
+++ metanit/BookStore/BookStore/Models/BookDbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace BookStore.Models
+{
+    public sealed class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Price = 220 });
+            db.Books.Add(new Book { Name = "Отцы и дети", Author = "И. Тургенев", Price = 180 });
+            db.Books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Price = 150 });
+
+            base.Seed(db);
+        }
+    }
+}
