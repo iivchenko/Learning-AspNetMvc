a9eb7c846319eb998ac28837d69e827fd4a13368
Ivan Ivchenko <ivan.ivchenko@betssongroup.com>
2018-03-29 09:57:26 +0300

    Controller study

---
+++ metanit/BookStore/BookStore/Controllers/TestController.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Mime;
+using System.Text;
+using System.Web.Mvc;
+using BookStore.Utils;
+
+namespace BookStore.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public string SayHello(string name, int times)
+        {
+            ViewBag
+            var hello = new StringBuilder();
+
+            for(var i = 0; i < times; i++)
+            {
+                hello.AppendLine($"<p>Hello {name}</p>");
+            }
+
+            return hello.ToString();
+        }
+
+        [HttpGet]
+        public ContentResult SayHello2(string name, int times)
+        {
+            var hello = new StringBuilder();
+
+            for (var i = 0; i < times; i++)
+            {
+                hello.AppendLine($"<p>Hello {name}</p>");
+            }
+
+            return Content(hello.ToString());
+        }
+
+        [HttpGet]
+        public ActionResult MyActionResult(string name)
+        {
+            return new MyHtmlResult(name);
+        }
+
+        [HttpGet]
+        public FileResult GetFile(string path)
+        {
+            return new FilePathResult(path, "application/octet-stream");
+        }
+    }
+}
---
+++ metanit/BookStore/BookStore/Utils/MyHtmlResult.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Web.Mvc;
+
+namespace BookStore.Utils
+{
+    public sealed class MyHtmlResult : ActionResult
+    {
+        private readonly string _body;
+
+        public MyHtmlResult(string body)
+        {
+            _body = body;
+        }
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var html = new StringBuilder();
+
+            html
+                .AppendLine("<!DOCTYPE html><html><head>")
+                .AppendLine("<title>Главная страница</title>")
+                .AppendLine("<meta charset=utf-8 />")
+                .AppendLine("</head> <body bgcolor=\"red\">")
+                .AppendLine(_body)
+                .AppendLine("</body></html>");
+
+            context
+                .HttpContext
+                .Response
+                .Write(html.ToString());
+        }
+    }
+}
